c9516675ed3530bbf0d1206088204e4a4597436a
svd110 <swatisahai110@gmail.com>
2026-03-30 13:42:08 -0400

    Drug DataLoader created and relevant classes updated

---
+++ src/model/ConditionConflict.java
@@ -0,0 +1,29 @@
+package model;
+
+public class ConditionConflict {
+	
+		private String condition;
+		private String severity;
+		private String description;
+		
+		public String getCondition() {
+			return this.condition;
+		}
+		public String getSeverity() {
+			return this.severity;
+		}
+		public String getDescription() {
+			return this.description;
+		}
+		public void setCondition(String condition) {
+			this.condition = condition;
+		}
+		public void setSeverity(String severity) {
+			this.severity = severity;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		
+		
+}
